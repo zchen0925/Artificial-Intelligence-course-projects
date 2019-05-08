b321ae6e2e3917ffa41a48389c75500ecfe868eb
Ziqi Chen <zc23@maroon21.cs.calvin.edu>
2019-05-07 22:38:46 -0400

    testing images

--- project/data_manipulation.py
+++ project/data_manipulation.py
@@ -1,4 +1,4 @@
-from nilearn import datasets
+from nilearn import datasets, image, plotting
 haxby_dataset = datasets.fetch_haxby()
 # 'func' is a list of filenames: one for each subject
 fmri_filename = haxby_dataset.func[0]
@@ -6,3 +6,5 @@ fmri_filename = haxby_dataset.func[0]
 # print basic information on the dataset
 print('First subject functional nifti images (4D) are at: %s' %
       fmri_filename)  # 4D data
+
+plotting.plot_glass_brain("/home/zc23/CS344/anat.nii")
